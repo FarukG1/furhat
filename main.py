e1fa6a779d911f6b21127fe493ee579cd10f4679
FarukG1 <faruk_fkg_02@outlook.de>
2024-04-17 21:38:44 +0200

    api test

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
+import json
 import requests
 
-url = "http://localhost:54321/furhat"  # Replace with your desired URL
-
-response = requests.get(url)
-
+# Test Connection
+response = requests.get("http://localhost:54321/furhat")
 print(response.text)
+
+# Get all gestures
+response = requests.get("http://localhost:54321/furhat/gestures")
+print(json.dumps(response.json(), indent=4))
+
+# Say something
+data = {
+    "text": "Test Hallo",
+    "url": "",
+    "blocking": False,
+    "lipsync": False,
+    "abort": False
+}
+response = requests.post("http://localhost:54321/furhat/say", json=data)
+print(json.dumps(response.json(), indent=4))
