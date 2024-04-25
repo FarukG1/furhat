a7db5f42b2a82cd987535a78123ad40727acde1e
mabrouk002 <mabrouk.abdelkafi@outlook.de>
2024-04-25 11:04:08 +0200

    Sleep

--- echobot.py
+++ echobot.py
@@ -1,10 +1,12 @@
 from furhat_remote_api import FurhatRemoteAPI
+import time
 
 # Create an instance of the FurhatRemoteAPI class, providing the address of the robot or the SDK running the virtual robot
 furhat = FurhatRemoteAPI("localhost")
 
 # Get the voices on the robot
 voices = furhat.get_voices()
+
 
 # Set the voice of the robot
 furhat.set_voice(name='Hans')
@@ -15,14 +17,17 @@ furhat.say(text="Hallo wer sind sie?")
 # Fokus auf den benutzer mit geringster entfernung
 furhat.attend(user="CLOSEST")
 
-while(True):
+while True:
+    time.sleep(2)
     # Listen to user speech and return ASR result
     result = furhat.listen(language="de-DE")
 
-    # Abbruchbedingung
-    if result.message == "Stopp":
+    # Check if the user wants to stop the conversation
+    if result.message == "stopp":
         print("User stopped the conversation")
         break
 
-    # Echo the user's speech
+    # Echo only the user's speech
     furhat.say(text=result.message)
+    
+furhat.say(text="Tschüss")
