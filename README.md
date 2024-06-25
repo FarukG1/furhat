cc33b228ff1e4f1ebdd9cd6f9321fb9caeab16a8
FarukG1 <faruk_fkg_02@outlook.de>
2024-06-25 16:27:09 +0200

    doc finished

--- README.md
+++ README.md
@@ -1,5 +1,57 @@
-# Installation
+# Furhat + ChatGPT (Maria Vogel)
+
+In diesem Projekt verbinden wir ChatGPT mit dem Furhat-Roboter, sodass ChatGPT durch Furhat hören und mit Personen kommunizieren kann. Wir schlagen damit eine Brücke zwischen der virtuellen und der realen Welt und nutzen Furhat als Schnittstelle. Die Aufgabe besteht darin, ChatGPT in "Maria Vogel" zu verwandeln, wofür wir einen speziellen Prompt erstellt haben.
+
+## Installation
+
+Zunächst erstellt man eine virtuelle Python-Umgebung. Nach deren Aktivierung können die benötigten Python-Pakete installiert werden. Befolgen Sie dazu die folgenden Schritte:
+
+Linux & MacOS:
 
 1. ```python -m venv ./venv```
 2. ```source ./venv/bin/activate```
 3. ```pip install -r requirements.txt```
+
+Windows:
+
+1. ```python -m venv ./venv```
+2. ```.\venv\Scripts\activate```
+3. ```pip install -r requirements.txt```
+
+## Configuration
+
+Für die Konfiguration werden zwei Elemente benötigt:
+
+1. **Prompt**: Dieser befindet sich in der Datei `prompt.txt`.
+2. **OpenAI API Key**: Dieser muss in einer Datei namens `.env` hinterlegt werden, wobei der Schlüssel `OPENAI_API_KEY` lauten sollte (Beispiel: `OPENAI_API_KEY=XYZ`).
+
+## Dokumentation
+
+Dieses Skript dient der Erstellung eines deutschsprachigen Chatbots namens "Maria Vogel", der auf dem OpenAI GPT-4 Modell und der Furhat Remote API basiert.
+
+### Abhängigkeiten
+
+- `furhat_remote_api`: Ermöglicht die Verbindung zur Furhat Remote API, um den Furhat-Roboter zu steuern.
+- `openai`: Stellt die Verbindung zur OpenAI API her, um das GPT-4 Modell zu nutzen.
+- `atexit`: Registriert eine Funktion, die beim Beenden des Skripts aufgerufen wird, um den erstellten Assistenten zu löschen.
+
+### Funktionen
+
+- `delete_assistant(assistant_id)`: Diese Funktion löscht den Assistenten mit der angegebenen ID und wird beim Beenden des Skripts automatisch aufgerufen.
+
+### Ausführung
+
+1. Das Skript liest eine Eingabeaufforderung aus der Datei `prompt.txt`.
+2. Es verbindet sich mit der Furhat Remote API und setzt die Stimme des Furhat-Roboters auf "Vicki".
+3. Es stellt eine Verbindung zur OpenAI API her.
+4. Ein Assistent namens "Maria Vogel" wird mittels der OpenAI API erstellt. Die Anweisungen für den Assistenten werden aus der Textdatei übernommen und das Modell "gpt-4o" wird verwendet.
+5. Ein Thread wird gestartet, um den Chatverlauf zu verwalten.
+6. Die Funktion `delete_assistant` wird registriert und beim Skriptende aufgerufen, um den Assistenten zu löschen. Die ID des Assistenten wird an die Funktion übergeben.
+7. Das Skript geht in eine Endlosschleife über und wartet auf Nachrichten vom Benutzer. Bei Empfang einer Nachricht wird diese an die OpenAI API gesendet, die eine Antwort generiert und diese wird dann vom Furhat-Roboter ausgesprochen.
+
+### Gruppenmitglieder
+
+- Mabrouk Abdelkafi
+- Ismail Adruich
+- Diyar Altin
+- Faruk-Kerim Güc
--- chatgpt.py
+++ chatgpt.py
@@ -4,7 +4,8 @@ import atexit
 import re
 
 # Terminal Farben
-class colors:
+class Colors:
+    """Farben für die Ausgabe im Terminal."""
     PURPLE = '\033[95m'
     BLUE = '\033[94m'
     CYAN = '\033[96m'
@@ -17,13 +18,13 @@ class colors:
 
 def colored(color, text):
     """Fügt Farbe zum Text hinzu."""
-    return f"{color}{text}{colors.RESET}"
+    return f"{color}{text}{Colors.RESET}"
 
 def programm_exit(assistant_id):
     """Löscht den Assistenten und setzt die LED Farbe auf schwarz."""
     client.beta.assistants.delete(assistant_id)
     furhat.set_led(red=0, green=0, blue=0)
-    print(colored(colors.PURPLE + colors.BOLD,"Status - Chat beendet"))
+    print(colored(Colors.PURPLE + Colors.BOLD,"Status - Chat beendet"))
 
 
 # Prompt aus Textdatei in Variable speichern
@@ -33,11 +34,11 @@ with open("./prompt.txt", "r", encoding="utf-8") as file:
 # Verbindung zur Furhat Remote API aufbauen
 furhat = FurhatRemoteAPI("localhost")
 furhat.set_voice(name="Vicki")
-print(colored(colors.PURPLE + colors.BOLD,"Status - Furhat Remote API verbunden"))
+print(colored(Colors.PURPLE + Colors.BOLD,"Status - Furhat Remote API verbunden"))
 
 # Verbindung zur OpenAI API aufbauen
 client = OpenAI()
-print(colored(colors.PURPLE + colors.BOLD,"Status - OpenAI API verbunden"))
+print(colored(Colors.PURPLE + Colors.BOLD,"Status - OpenAI API verbunden"))
 
 # Assistant erstellen (Für Chatverlauf)
 assistant = client.beta.assistants.create(
@@ -45,24 +46,24 @@ assistant = client.beta.assistants.create(
     instructions=prompt,
     model="gpt-4o",
 )
-print(colored(colors.PURPLE + colors.BOLD,"Status - Assistant erstellt"))
+print(colored(Colors.PURPLE + Colors.BOLD,"Status - Assistant erstellt"))
 
 # Thread erstellen (Fürs bearbeiten des Chatverlaufs)
 thread = client.beta.threads.create()
-print(colored(colors.PURPLE + colors.BOLD,"Status - Thread erstellt"))
+print(colored(Colors.PURPLE + Colors.BOLD,"Status - Thread erstellt"))
 
-print(colored(colors.PURPLE + colors.BOLD,"Status - Chat beginnt"))
+print(colored(Colors.PURPLE + Colors.BOLD,"Status - Chat beginnt"))
 atexit.register(programm_exit, assistant.id)
 while True:
     # Fokus auf den Benutzer mit geringster Entfernung
     furhat.attend(user="CLOSEST")
 
     # Nutzer zuhören
-    print(colored(colors.GREEN,"\nFurhat - Zuhören"))
+    print(colored(Colors.GREEN,"\nFurhat - Zuhören"))
     furhat.set_led(red=0, green=255, blue=0)
     request = furhat.listen(language="de-DE")
     while not request.message:
-        print(colored(colors.ERROR,"Furhat - Nutzer hat nichts gesagt, es wird erneut zugehört"))
+        print(colored(Colors.ERROR,"Furhat - Nutzer hat nichts gesagt, es wird erneut zugehört"))
         request = furhat.listen(language="de-DE")
 
     furhat.set_led(red=255, green=0, blue=0)
@@ -91,12 +92,12 @@ while True:
 
                 # Geste ausführen
                 response_gesture = response_splitted[index]
-                print(colored(colors.CYAN,"\nFurhat - Gestikuliert: " + response_gesture))
+                print(colored(Colors.CYAN,"\nFurhat - Gestikuliert: " + response_gesture))
                 furhat.gesture(name=response_gesture, blocking=False)
 
                 # Antwort ausgeben
                 response_text = response_splitted[index + 1]
-                print(colored(colors.BLUE,"Furhat - Spricht: " + response_text))
+                print(colored(Colors.BLUE,"Furhat - Spricht: " + response_text))
                 furhat.say(text=response_text, blocking=True)
 
                 index += 2
@@ -104,5 +105,5 @@ while True:
                 loop = False
     else:
         # Antwort ausgeben
-        print(colored(colors.BLUE,"\nFurhat - Spricht: " + response))
+        print(colored(Colors.BLUE,"\nFurhat - Spricht: " + response))
         furhat.say(text=response, blocking=True)
